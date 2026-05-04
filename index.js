3ea5c6098a67942c46e009690d3fe51ed6e5a4b0
JesusLeos16 <jelm060716@gmail.com>
2026-05-04 16:00:44 -0600

    Sube configuracion de login vista en clase

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
+const cors = require("cors");
 const academiaRoutes = require("./routes/academia");
 const combateRoutes = require("./routes/combate");
 const torneoRoutes = require("./routes/torneo");
 const peleadorRoutes = require("./routes/peleador");
 const matchupRoutes = require("./routes/matchup");
+const authRoutes = require("./auth");
 const db = require('./config/db')
+app.use(cors());
+app.use(express.json());
+app.use("/auth", authRoutes);
 app.use("/academia", academiaRoutes);
 app.use("/torneo", torneoRoutes);
 app.use("/combate", combateRoutes);
@@ -16,6 +20,9 @@ const testDBConnectionAndStart = async()=>{
   try {
     await db.query ('SELECT 1')
     console.log('Ta funcionando')
+    app.listen(3000, () => {
+      console.log("Servidor corriendo en http://localhost:3000");
+    });
   } catch (error) {
     console.error('tas mal con la DB',error.message)
     process.exit(1)
@@ -23,6 +30,3 @@ const testDBConnectionAndStart = async()=>{
 }
 testDBConnectionAndStart();
 
-app.listen(3000, () => {
-  console.log("Servidor corriendo en http://localhost:3000");
-});
