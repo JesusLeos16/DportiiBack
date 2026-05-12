4a1178943ba5d4c178e3b4154349e42417a3c58b
JesusLeos16 <jelm060716@gmail.com>
2026-05-12 16:00:01 -0600

    Configura puerto dinamico en index.js para deploy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const torneoRoutes = require("./routes/torneo");
 const peleadorRoutes = require("./routes/peleador");
 const matchupRoutes = require("./routes/matchup");
 const authRoutes = require("./auth");
-const db = require('./config/db')
+const db = require("./config/db");
 app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoutes);
@@ -16,17 +16,25 @@ app.use("/torneo", torneoRoutes);
 app.use("/combate", combateRoutes);
 app.use("/peleador", peleadorRoutes);
 app.use("/matchup", matchupRoutes);
-const testDBConnectionAndStart = async()=>{
+const testDBConnectionAndStart = async () => {
   try {
-    await db.query ('SELECT 1')
-    console.log('Ta funcionando')
-    app.listen(3000, () => {
-      console.log("Servidor corriendo en http://localhost:3000");
+    //   await db.query ('SELECT 1')
+    //   console.log('Ta funcionando')
+    //   app.listen(3000, () => {
+    //     console.log("Servidor corriendo en http://localhost:3000");
+    //   });
+    // } catch (error) {
+    //   console.error('tas mal con la DB',error.message)
+    //   process.exit(1)
+    // }
+
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en el puerto ${PORT}`);
     });
   } catch (error) {
-    console.error('tas mal con la DB',error.message)
-    process.exit(1)
+    console.error("Hay algo mal en el backend mi chavo:", error.message);
+    process.exit(1);
   }
-}
+};
 testDBConnectionAndStart();
-
